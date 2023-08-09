47db8cf723132607fa7c04be658b404382a501af
RyuShige <66407509+RyuShige@users.noreply.github.com>
2023-08-09 15:32:05 +0900

    毎エポックごとにwandbにロスを送信

--- main_repeat.py
+++ main_repeat.py
@@ -144,6 +144,8 @@ if __name__ == '__main__':
             for param in model.item_emb.parameters(): loss += args.l2_emb * torch.norm(param)
             loss.backward()
             adam_optimizer.step()
+            wandb.log({"epoch": epoch, "loss": loss.item()})
+
     
         if epoch % 1 == 0:
             model.eval()
