938b3744d8f4d075cd33410fad5f5473b7201941
RyuShige <66407509+RyuShige@users.noreply.github.com>
2023-10-09 15:35:20 +0900

    lightsansに単純にRepeatを加算する方法を追加

--- main_repeat_for_like_recbole.py
+++ main_repeat_for_like_recbole.py
@@ -12,6 +12,7 @@ from model_like_recbole import SASRec
 from sasrec_repeat_like_recbole import SASRec_Repeat
 from sasrec_repeat_plus_like_recbole import SASRec_RepeatPlus
 from light_sans import LightSANs
+from light_sans_simple_repeat import LightSANs_simple_R
 from light_sans_repeat import LightSANs_Repeat
 from utils import *
 
@@ -116,6 +117,8 @@ if __name__ == '__main__':
         model = SASRec_RepeatPlus(sessionsetnum, itemnum, repeatnum, args).to(args.device)
     elif args.model == 'LightSANs':
         model = LightSANs(sessionsetnum, itemnum, repeatnum, args).to(args.device)
+    elif args.model == 'LightSANs_simple_R':
+        model = LightSANs_simple_R(sessionsetnum, itemnum, repeatnum, args).to(args.device)
     elif args.model == 'LightSANs_Repeat':
         model = LightSANs_Repeat(sessionsetnum, itemnum, repeatnum, args).to(args.device)
     
@@ -174,7 +177,7 @@ if __name__ == '__main__':
             # u, seq, repeat, pos, neg = expand_samples(u, seq, repeat, pos, neg, args.maxlen)
             if args.model == 'SASRec':
                 pos_logits, neg_logits = model(ss, seq, pos, neg)
-            elif args.model == 'SASRec_Repeat' or args.model == 'SASRec_RepeatPlus' or args.model == 'LightSANs' or args.model == 'LightSANs_Repeat':
+            elif args.model == 'SASRec_Repeat' or args.model == 'SASRec_RepeatPlus' or args.model == 'LightSANs' or args.model == 'LightSANs_simple_R' or args.model == 'LightSANs_Repeat':
                 pos_logits, neg_logits = model(ss, seq, repeat, pos, neg)
             pos_labels, neg_labels = torch.ones(pos_logits.shape, device=args.device), torch.zeros(neg_logits.shape, device=args.device)
             # print("\neye ball check raw_logits:"); print(pos_logits); print(neg_logits) # check pos_logits > 0, neg_logits < 0
@@ -238,6 +241,9 @@ if __name__ == '__main__':
                 elif args.model == 'LightSANs':
                     fname = 'LightSANs_BestModel.MRR={}.epoch={}.lr={}.layer={}.head={}.hidden={}.maxlen={}.pth'
                     fname = fname.format(early_stop, best_epoch, args.lr, args.num_blocks, args.num_heads, args.hidden_units, args.maxlen)
+                elif args.model == 'LightSANs_simple_R':
+                    fname = 'LightSANs_simple_R_BestModel.MRR={}.epoch={}.lr={}.layer={}.head={}.hidden={}.maxlen={}.pth'
+                    fname = fname.format(early_stop, best_epoch, args.lr, args.num_blocks, args.num_heads, args.hidden_units, args.maxlen)
                 elif args.model == 'LightSANs_Repeat':
                     fname = 'LightSANs_Repeat_BestModel.MRR={}.epoch={}.lr={}.layer={}.head={}.hidden={}.maxlen={}.pth'
                     fname = fname.format(early_stop, best_epoch, args.lr, args.num_blocks, args.num_heads, args.hidden_units, args.maxlen)
@@ -277,6 +283,9 @@ if __name__ == '__main__':
                 fname = fname.format(best_epoch, args.lr, args.num_blocks, args.num_heads, args.hidden_units, args.maxlen)
             elif args.model == 'LightSANs':
                 fname = 'LightSANs.epoch={}.lr={}.layer={}.head={}.hidden={}.maxlen={}.pth'
+                fname = fname.format(best_epoch, args.lr, args.num_blocks, args.num_heads, args.hidden_units, args.maxlen)
+            elif args.model == 'LightSANs_simple_R':
+                fname = 'LightSANs_simple_R.epoch={}.lr={}.layer={}.head={}.hidden={}.maxlen={}.pth'
                 fname = fname.format(best_epoch, args.lr, args.num_blocks, args.num_heads, args.hidden_units, args.maxlen)
             elif args.model == 'LightSANs_Repeat':
                 fname = 'LightSANs_Repeat.epoch={}.lr={}.layer={}.head={}.hidden={}.maxlen={}.pth'
